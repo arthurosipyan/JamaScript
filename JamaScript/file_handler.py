2c48aa79c879e9656eba728dcf0f2ef22473914b
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2018-10-18 16:15:47 -0400

    Update file_handler.py

--- JamaScript/file_handler.py
+++ JamaScript/file_handler.py
@@ -1,38 +1,38 @@
 import pandas as pd
+from easygui import fileopenbox
+
+
+def get_file():
+    return fileopenbox(msg="Please select the excel file you'd wish to migrate", title="JamaScript", default='*', filetypes=".xlsx", multiple=False)
 
 
 def root_components_file():
     while True:
-        root_cmp_file_name = input("Enter file: ")
-        print("______________________________________________________\n")
+        root_cmp_file_name = get_file()
         try:
             cmp_excel_file = pd.ExcelFile(root_cmp_file_name)
             df = cmp_excel_file.parse("Sheet1")
             return df.iloc[:, 0].tolist()
         except FileNotFoundError:
-            print("Error: File not found\n"
-                  "______________________________________________________\n")
+            print("Error: File not found")
             root_components_file()
 
 
 def components_file():
     while True:
-        cmp_file_name = input("Enter file: ")
-        print("______________________________________________________\n")
+        cmp_file_name = get_file()
         try:
             cmp_excel_file = pd.ExcelFile(cmp_file_name)
             df = cmp_excel_file.parse("Sheet1")
             return df.iloc[:, 0].tolist()
         except FileNotFoundError:
-            print("Error: File not found\n"
-                  "______________________________________________________\n")
+            print("Error: File not found")
             components_file()
 
 
 def use_cases_file():
     while True:
-        uc_file_name = input("Enter file: ")
-        print("______________________________________________________\n")
+        uc_file_name = get_file()
         try:
             uc_excel_file = pd.ExcelFile(uc_file_name)
             df = uc_excel_file.parse("Sheet1")
@@ -45,15 +45,13 @@ def use_cases_file():
             uc_data["Blueprint_ID"] = df.iloc[:, 5].tolist()
             return uc_data
         except FileNotFoundError:
-            print("Error: File not found\n"
-                  "______________________________________________________\n")
+            print("Error: File not found")
             use_cases_file()
 
 
 def wireframes_file():
     while True:
-        wf_file_name = input("Enter file: ")
-        print("______________________________________________________\n")
+        wf_file_name = get_file()
         try:
             wf_excel_file = pd.ExcelFile(wf_file_name)
             df = wf_excel_file.parse("Sheet1")
@@ -62,15 +60,13 @@ def wireframes_file():
             wf_data["Blueprint_ID"] = df.iloc[:, 1].tolist()
             return wf_data
         except FileNotFoundError:
-            print("Error: File not found\n"
-                  "______________________________________________________\n")
+            print("Error: File not found")
             wireframes_file()
 
 
 def requirements_file():
     while True:
-        rq_file_name = input("Enter file: ")
-        print("______________________________________________________\n")
+        rq_file_name = get_file()
         try:
             rq_excel_file = pd.ExcelFile(rq_file_name)
             df = rq_excel_file.parse("Sheet1")
@@ -80,15 +76,13 @@ def requirements_file():
             rq_data["Blueprint_ID"] = df.iloc[:, 2].tolist()
             return rq_data
         except FileNotFoundError:
-            print("Error: File not found\n"
-                  "______________________________________________________\n")
+            print("Error: File not found")
             requirements_file()
 
 
 def documents_file():
     while True:
-        rq_file_name = input("Enter file: ")
-        print("______________________________________________________\n")
+        rq_file_name = get_file()
         try:
             rq_excel_file = pd.ExcelFile(rq_file_name)
             df = rq_excel_file.parse("Sheet1")
@@ -98,6 +92,5 @@ def documents_file():
             doc_data["Blueprint_ID"] = df.iloc[:, 2].tolist()
             return doc_data
         except FileNotFoundError:
-            print("Error: File not found\n"
-                  "______________________________________________________\n")
+            print("Error: File not found")
             documents_file()
