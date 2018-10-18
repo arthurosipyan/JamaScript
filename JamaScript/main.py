6a0f82fb82bc0a47ec62a537c9101897a25a09cb
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2018-10-18 16:00:24 -0400

    EasyGui Implementation
    Needs testing and additional thrown exceptions.

--- JamaScript/main.py
+++ JamaScript/main.py
@@ -1,8 +1,13 @@
 import json
 import requests
+from easygui import choicebox, enterbox
 
 from helper import project_api_id, access_token
 
+title = "JamaScript"
+display = ""
+artifact_result = ""
+main_components_result = ""
 component_ids = []
 test_management_ids = []
 target_item_id = 0
@@ -15,10 +20,9 @@ headers = {"Authorization": "Bearer " + access_token, "Content-Type": "applicati
 
 
 def add_components_to_root_level(root_cmp_names1):
-    global original_count
+    global original_count, main_components_result
     original_count = 9 * len(root_cmp_names1)
     url = "https://fitchratings.jamacloud.com/rest/latest/items?setGlobalIdManually=false"
-    print("*** Attempting to create [" + str(original_count) + "] items...")
     for name in root_cmp_names1:
         data = {"project": project_api_id, "itemType": 30,
                 "childItemType": 30, "fields": {"name": name}}
@@ -26,7 +30,8 @@ def add_components_to_root_level(root_cmp_names1):
         response = requests.request("POST", url, headers=headers, data=json.dumps(data))
         global component_ids
         component_ids.append(response.json()["meta"]["id"])
-    print("*** [" + str(len(component_ids)) + "/" + str(len(root_cmp_names1)) + "] Components created")
+    main_components_result += ("[" + str(len(component_ids)) + "/" + str(len(root_cmp_names1)) + "] Components "
+                                                                                                 "created\n")
     global total_items_imported
     total_items_imported += len(component_ids)
 
@@ -53,11 +58,14 @@ def add_sets_to_components():
             response = requests.request("POST", url, headers=headers, data=json.dumps(data))
             count.append(response.json()["meta"]["id"])
         i += 1
-    print("*** [" + str(5 * len(component_ids)) + "/" + str(len(count)) + "] sets of Use Cases, Functional "
-                                                                          "Requirements, "
-                                                                          "Non-Functional Requirements, Technical "
-                                                                          "Requirements, "
-                                                                          "and Wireframes created")
+    global main_components_result
+    main_components_result += ("[" + str(5 * len(component_ids)) + "/" + str(len(count)) + "] Sets of Use Cases, "
+                                                                                           "Functional "
+                                                                                           "Requirements, "
+                                                                                           "Non-Functional "
+                                                                                           "Requirements, Technical "
+                                                                                           "Requirements, "
+                                                                                           "and Wireframes created\n")
     global total_items_imported
     total_items_imported += (5 * len(component_ids))
 
@@ -70,8 +78,11 @@ def add_test_management():
         response = requests.request("POST", url, headers=headers, data=json.dumps(data))
         global test_management_ids
         test_management_ids.append(response.json()["meta"]["id"])
-    print("*** [" + str(len(component_ids)) + "/" + str(len(test_management_ids)) + "] Test Management Sub Components "
-                                                                                    "created")
+    global main_components_result
+    main_components_result += ("[" + str(len(component_ids)) + "/" + str(len(test_management_ids)) + "] Test "
+                                                                                                     "Management Sub "
+                                                                                                     "Components "
+                                                                                                     "created\n")
     global total_items_imported
     total_items_imported += len(component_ids)
 
@@ -96,58 +107,27 @@ def add_test_cases_and_defects():
             response = requests.request("POST", url, headers=headers, data=json.dumps(data))
             count.append(response.json()["meta"]["id"])
         i += 1
-    print("*** [" + str(len(count)) + "/" + str(2 * len(component_ids)) + "] sets of Test Cases and Defects created")
-    print("______________________________________________________")
-    global total_items_imported
+    global main_components_result, total_items_imported
+    main_components_result += ("[" + str(len(count)) + "/" + str(2 * len(component_ids)) + "] Sets of Test Cases and "
+                                                                                           "Defects created\n")
     total_items_imported += (2 * len(component_ids))
     if original_count == total_items_imported:
-        print("*** Success, imported [" + str(total_items_imported) + "] items!")
+        main_components_result += ("\nSuccess, imported [" + str(total_items_imported) + "] items!")
     else:
-        print("*** Failed, only [" + str(total_items_imported) + "] items were imported.")
-        exit()
-
-
-def confirm_artifact_choice():
-    user_confirm = input("Is this the artifact you'd like to add? (y/n) ")
-    print("______________________________________________________\n")
-    if user_confirm.lower() == 'n':
-        get_user_choices()
-    elif user_confirm.lower() != 'y':
-        print("Error: Please enter a valid option.")
-        print("______________________________________________________\n")
-        confirm_artifact_choice()
+        main_components_result += ("Failed, only [" + str(total_items_imported) + "] items were imported.")
 
 
 def get_user_choices():
-    print("______________________________________________________\n"
-          "What artifact type would you like to add?\n"
-          "______________________________________________________\n"
-          "[1] Component\n"
-          "[2] Use Case\n"
-          "[3] Requirement\n"
-          "[4] Wireframe\n"
-          "[5] Document\n"
-          "[6] Go back\n"
-          "[7] Exit\n"
-          "______________________________________________________\n")
+    choices = ["Component", "Use Case", "Requirement", "Wireframe", "Document", "Go back"]
     global user_choice
-    user_choice = int(input("______________________________________________________\n"
-                            "Enter the number choice: "))
-    if user_choice == 7:
-        quit()
-    elif user_choice == 6:
+    user_choice = choicebox("What artifact type would you like to add?", choices=choices)
+    if user_choice == "Go back":
         main()
-    elif user_choice not in range(1, 7):
-        print("______________________________________________________\n"
-              "Error: Please enter a valid number.\n"
-              "______________________________________________________\n")
-        get_user_choices()
     else:
-        confirm_artifact_choice()
-        document_key = input("Enter item's ID: ")
-        print("______________________________________________________\n")
+        msg = "Please enter the target item's ID"
+        document_key = enterbox(msg, title)
         url = "https://fitchratings.jamacloud.com/rest/latest/abstractitems?project=" \
-              + project_api_id + "&documentKey=" + document_key
+              + str(project_api_id) + "&documentKey=" + document_key
         response = requests.request("GET", url, headers=headers)
         global target_item_id
         target_item_id = response.json()["data"][0]['id']
@@ -155,7 +135,6 @@ def get_user_choices():
 
 def add_uc(uc_data1):
     count = []
-    print("*** Attempting to import [" + str(len(uc_data1["Name"])) + "] Use Cases...")
     url = "https://fitchratings.jamacloud.com/rest/latest/items?setGlobalIdManually=false"
     for name, pre, main_flow, post, alt, blueprint_id in zip(uc_data1["Name"], uc_data1["PreCondition"],
                                                              uc_data1["MainFlow"],
@@ -174,15 +153,15 @@ def add_uc(uc_data1):
                 }
         response = requests.request("POST", url, headers=headers, data=json.dumps(data))
         count.append(response.json()["meta"]["id"])
+    global artifact_result
     if len(uc_data1["Name"]) == len(count):
-        print("*** Success, imported [" + str(len(count)) + "] Use Cases!")
+        artifact_result = "Success, imported " + str(len(count)) + " Use Cases!"
     else:
-        print("*** Failed, only [" + str(len(count)) + "] Use Cases imported.")
+        artifact_result = "Failed, only " + str(len(count)) + " Use Cases imported"
 
 
 def add_cmp(cmp_name1):
     count = []
-    print("*** Attempting to import [" + str(len(cmp_name1)) + "] Components...")
     url = "https://fitchratings.jamacloud.com/rest/latest/items?setGlobalIdManually=false"
     for name in cmp_name1:
         data = {"project": project_api_id, "itemType": 30,
@@ -196,15 +175,15 @@ def add_cmp(cmp_name1):
                 }
         response = requests.request("POST", url, headers=headers, data=json.dumps(data))
         count.append(response.json()["meta"]["id"])
+    global artifact_result
     if len(cmp_name1) == len(count):
-        print("*** Success, imported [" + str(len(count)) + "] Components!")
+        artifact_result = "Success, imported [" + str(len(count)) + "] Components!"
     else:
-        print("*** Failed, only [" + str(len(count)) + "] Components imported.")
+        artifact_result = "Failed, only [" + str(len(count)) + "] Components imported."
 
 
 def add_rq(rq_data1):
     count = []
-    print("*** Attempting to import [" + str(len(rq_data1["Name"])) + "] Requirements...")
     url = "https://fitchratings.jamacloud.com/rest/latest/items?setGlobalIdManually=false"
     for name, description, blueprint_id in zip(rq_data1["Name"], rq_data1["Description"], rq_data1["Blueprint_ID"]):
         data = {"project": project_api_id, "itemType": 87,
@@ -218,15 +197,15 @@ def add_rq(rq_data1):
                 }
         response = requests.request("POST", url, headers=headers, data=json.dumps(data))
         count.append(response.json()["meta"]["id"])
+    global artifact_result
     if len(rq_data1["Name"]) == len(count):
-        print("*** Success, imported [" + str(len(count)) + "] Requirements!")
+        artifact_result = "Success, imported [" + str(len(count)) + "] Requirements!"
     else:
-        print("*** Failed, only [" + str(len(count)) + "] Requirements were imported.")
+        artifact_result = "Failed, only [" + str(len(count)) + "] Requirements were imported."
 
 
 def add_document(doc_data1):
     count = []
-    print("*** Attempting to import [" + str(len(doc_data1["Name"])) + "] Documents...")
     url = "https://fitchratings.jamacloud.com/rest/latest/items?setGlobalIdManually=false"
     for name, description, blueprint_id in zip(doc_data1["Name"], doc_data1["Description"], doc_data1["Blueprint_ID"]):
         data = {"project": project_api_id, "itemType": 87,
@@ -240,15 +219,15 @@ def add_document(doc_data1):
                 }
         response = requests.request("POST", url, headers=headers, data=json.dumps(data))
         count.append(response.json()["meta"]["id"])
+    global artifact_result
     if len(doc_data1["Name"]) == len(count):
-        print("*** Success, imported [" + str(len(count)) + "] Documents!")
+        artifact_result = "Success, imported [" + str(len(count)) + "] Documents!"
     else:
-        print("*** Failed, only [" + str(len(count)) + "] Documents imported.")
+        artifact_result = "Failed, only [" + str(len(count)) + "] Documents imported."
 
 
 def add_wf(wf_data1):
     count = []
-    print("*** Attempting to import [" + str(len(wf_data1["Name"])) + "] Wireframes...")
     url = "https://fitchratings.jamacloud.com/rest/latest/items?setGlobalIdManually=false"
     for name, blueprint_id in zip(wf_data1["Name"], wf_data1["Blueprint_ID"]):
         data = {"project": project_api_id, "itemType": 116,
@@ -262,32 +241,39 @@ def add_wf(wf_data1):
                 }
         response = requests.request("POST", url, headers=headers, data=json.dumps(data))
         count.append(response.json()["meta"]["id"])
+    global artifact_result
     if len(wf_data1["Name"]) == len(count):
-        print("*** Success, imported [" + str(len(count)) + "] Wireframes!")
+        artifact_result = "Success, imported [" + str(len(count)) + "] Wireframes!"
     else:
-        print("*** Failed, only [" + str(len(count)) + "] Wireframes imported.")
+        artifact_result = "Failed, only [" + str(len(count)) + "] Wireframes imported."
 
 
 def add_requirements():
-    if user_choice == 1:
+    global display
+    if user_choice == "Component":
         from file_handler import components_file
         add_cmp(components_file())
+        display = artifact_result
         main2()
-    elif user_choice == 2:
+    elif user_choice == "Use Case":
         from file_handler import use_cases_file
         add_uc(use_cases_file())
+        display = artifact_result
         main2()
-    elif user_choice == 3:
+    elif user_choice == "Requirement":
         from file_handler import requirements_file
         add_rq(requirements_file())
+        display = artifact_result
         main2()
-    elif user_choice == 4:
+    elif user_choice == "Wireframe":
         from file_handler import wireframes_file
         add_wf(wireframes_file())
+        display = artifact_result
         main2()
-    elif user_choice == 5:
+    elif user_choice == "Document":
         from file_handler import documents_file
         add_document(documents_file())
+        display = artifact_result
         main2()
     else:
         print("Error")
@@ -295,69 +281,42 @@ def add_requirements():
 
 
 def main2():
+    choices = ["Add artifacts", "Change project"]
     global main2_selection
-    main2_selection = int(input("______________________________________________________\n"
-                                "_________ What else would you like to do? ____________\n"
-                                "______________________________________________________\n"
-                                "[1] Add artifacts\n"
-                                "[2] Change project\n"
-                                "[3] Exit\n"
-                                "______________________________________________________\n"))
-
-    if main2_selection == 1:
+    main2_selection = choicebox(display + "\n\n What else would you like to do?", choices=choices, title=title)
+    if main2_selection == "Add artifacts":
         get_user_choices()
         add_requirements()
-    elif main2_selection == 2:
+    elif main2_selection == "Change project":
         from helper import get_project_name
         get_project_name()
         main()
-    elif main2_selection == 3:
-        quit()
     else:
-        print("______________________________________________________\n"
-              "Error: Please enter a valid number.\n"
-              "______________________________________________________\n")
-        main2()
+        exit()
 
 
 def main():
-    global main_selection
-    main_selection = int(input("_____________ What would you like to do? _____________\n"
-                               "______________________________________________________\n"
-                               "[1] Add main components with their sets\n"
-                               "[2] Add artifacts\n"
-                               "[3] Change project\n"
-                               "[4] Exit\n"
-                               "______________________________________________________\n"))
-    print("______________________________________________________")
-
-    if main_selection == 1:
-        main_cmp_confirm = input("Would you like to add main components and their sets (y/n): ")
-        if main_cmp_confirm == 'y':
-            from file_handler import root_components_file
-            add_components_to_root_level(root_components_file())
-            add_sets_to_components()
-            add_test_management()
-            add_test_cases_and_defects()
-            main2()
-        elif main_cmp_confirm == 'n':
-            main()
-        else:
-            print("Please enter a valid option (y/n)")
-            main()
-    elif main_selection == 2:
+    choices = ["Add main components with their sets", "Add artifacts", "Change project"]
+    global main_selection, display
+    main_selection = choicebox("What would you like to do?", choices=choices, title=title)
+    if main_selection == "Add main components with their sets":
+        from file_handler import root_components_file
+        add_components_to_root_level(root_components_file())
+        add_sets_to_components()
+        add_test_management()
+        add_test_cases_and_defects()
+        display = main_components_result
+        main2()
+    elif main_selection == "Add artifacts":
         get_user_choices()
         add_requirements()
-    elif main_selection == 3:
+        display = artifact_result
+    elif main_selection == "Change project":
         from helper import get_project_name
         get_project_name()
         main()
-    elif main_selection == 4:
-        quit()
     else:
-        print("Error: Please enter a valid number.\n"
-              "______________________________________________________\n")
-        main()
+        exit()
 
 
 if __name__ == '__main__':
